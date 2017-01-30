e68adfd6860198b51804f85496d7e98b8e5997b1
Vladimir Apraxin <turbo.headcrab.666@gmail.com>
2017-01-31 00:34:35 +0300

    Initial commit

---
+++ src/ru/kpfu/rns/Main.java
@@ -0,0 +1,36 @@
+package ru.kpfu.rns;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String fileWithRandomNumbers = "files/file_with_random_numbers.txt";
+        String fileWithUniqueNumbers = "files/file_with_unique_numbers.txt";
+        try {
+            createFileAndFillWithRandomNumbers(500, fileWithRandomNumbers);
+            createFileWithUniqueNumbersFromOtherFile(fileWithUniqueNumbers, fileWithRandomNumbers);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createFileAndFillWithRandomNumbers(int sequenceLength, String filePath) throws IOException {
+        List<String> lines = new ArrayList<>();
+        for (int i = 0; i < sequenceLength; i++) {
+            Integer randomNumber = new Random().nextInt(151);
+            lines.add(randomNumber.toString());
+        }
+        Files.write(Paths.get(filePath), lines);
+    }
+
+    private static void createFileWithUniqueNumbersFromOtherFile(String newFilePath, String otherFilePath)
+            throws IOException {
+        List<String> listOfLines = Files.readAllLines(Paths.get(otherFilePath));
+        Set<String> setOfLines = new HashSet<>(listOfLines);
+        Files.write(Paths.get(newFilePath), setOfLines);
+    }
+}
